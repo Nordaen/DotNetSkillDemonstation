1339f68307e06727f74d638206a75414b45b5a93
Vlad Kondratev <sykidayteadres@gmail.com>
2020-08-18 22:33:13 +0300

    Added Count of binary int function
    Added middle-diffuculty task - count the amount of ones in the binary representation of an integer. So for example, since 12 is '1100' in binary, the return value should be 2.

---
+++ EdabitCompletedTasks/EdabitLibrary.Tests/Tasks/CountBinaryIntegersTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EdabitLibrary.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitLibrary.Tasks.Tests
+{
+    [TestClass()]
+    public class CountBinaryIntegersTests
+    {
+        [TestMethod()]
+        public void ToBinaryTest()
+        {
+            var task = new CountBinaryIntegers();
+            int x = 12;
+            int expected = 2;
+            int actual = task.CountIntBinary(x);
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ EdabitCompletedTasks/EdabitLibrary/Tasks/CountBinaryIntegers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdabitLibrary.Tasks
+{
+   public  class CountBinaryIntegers : EdabitTask
+    {
+        public CountBinaryIntegers()
+        {
+            Name = "Count Ones in Binary Representation of Integer";
+            Description = "Count the amount of ones in the binary representation of an integer. So for example, since 12 is '1100' in binary, the return value should be 2.";
+        }
+
+
+        public int CountIntBinary(int Number)
+        {
+            string result = Convert.ToString(Convert.ToInt32(Number.ToString(), 10), 2);
+            return  result.Where(x => x == '1').Count();
+
+        }
+    }
+}
