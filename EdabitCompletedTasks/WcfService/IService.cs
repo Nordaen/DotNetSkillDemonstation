ae0c5c638c538d4e7e384952f06e8034c48785a3
Vlad Kondratev <sykidayteadres@gmail.com>
2020-08-24 20:59:24 +0300

    Added WCF service and first function

---
+++ EdabitCompletedTasks/WcfService/IService.cs
@@ -0,0 +1,22 @@
+﻿using EdabitLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    [ServiceContract]
+    public interface IService
+    {
+
+        [OperationContract]
+        List<EdabitTask> GetTasks();
+    }
+
+
+   
+}
