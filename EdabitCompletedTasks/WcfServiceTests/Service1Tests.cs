57c0575d309a375b6aff23344c779738119b90bc
Vlad Kondratev <sykidayteadres@gmail.com>
2020-08-25 21:03:32 +0300

    Added test project for WCF service

---
+++ EdabitCompletedTasks/WcfServiceTests/Service1Tests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void GetTasksTest()
+        {
+            Service1 serv = new Service1();
+            var list = serv.GetTasks();
+            if (list == null && list.Count < 0)
+                Assert.Fail();
+
+        }
+    }
+}
